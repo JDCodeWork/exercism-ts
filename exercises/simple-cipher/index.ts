79a3f875d91a6d4b4af6483a581e0ec07ca34c5e
Juan Orozco | JDCodeWork <jdorozco792006@gmail.com>
2025-04-11 17:21:21 -0500

    feat: :construction: first concept implementation
    first non funcional implementation of simple-cipher

--- exercises/simple-cipher/index.ts
+++ exercises/simple-cipher/index.ts
@@ -1,15 +1,84 @@
 export class SimpleCipher {
+  private readonly FIRST_CHAR_CODE = 97
+  private readonly LAST_CHAR_CODE = 122
+
   key: string
 
   constructor(key?: string) {
-    this.key = key ?? "random_key"
+    if (key) {
+      this.key = key
+    } else {
+      // Generate random integers between 97 and 123 (excluding 123)
+      const randCodes = Array.from({ length: 100 }).map(() =>
+        Math.floor(Math.random() * (
+          this.LAST_CHAR_CODE + 1 - this.FIRST_CHAR_CODE
+        ) + this.FIRST_CHAR_CODE)
+      )
+
+      // Transforms random integers into chars and joins them into a single string
+      const randKey = randCodes.map(code => String.fromCharCode(code)).join("")
+
+      this.key = randKey
+    }
   }
 
-  encode(_text: string): string {
-    throw new Error('Remove this statement and implement this function')
+  encode(text: string): string {
+    const charCodes = this.transformTextToCodes(text)
+    const positionsToMove = this.calculatePositionsToMove(charCodes)
+
+    const encodedCodes = charCodes.map((code, idx) => {
+      const encodedCode = code + positionsToMove[idx]
+
+      if (encodedCode > this.LAST_CHAR_CODE) {
+        return encodedCode % this.LAST_CHAR_CODE + this.FIRST_CHAR_CODE
+      }
+
+      return encodedCode
+    })
+
+    return encodedCodes.map(code => String.fromCharCode(code)).join("")
   }
 
-  decode(_text: string): string {
-    throw new Error('Remove this statement and implement this function')
+  decode(text: string): string {
+    const charCodes = this.transformTextToCodes(text)
+    const positionsToMove = this.calculatePositionsToMove(charCodes)
+
+    const encodedCodes = charCodes.map((code, idx) => {
+      const encodedCode = code - positionsToMove[idx]
+
+      if (encodedCode < this.FIRST_CHAR_CODE) {
+        return this.LAST_CHAR_CODE - encodedCode
+      }
+
+      return encodedCode
+    })
+
+    return encodedCodes.map(code => String.fromCharCode(code)).join("")
+  }
+
+  private transformTextToCodes(text: string) {
+    return Array.from(text.toLocaleLowerCase())
+      .map(char => char.charCodeAt(0))
+      .filter(char => char != 32)
+  }
+
+  private calculatePositionsToMove(charCodes: Array<number>) {
+    return Array.from({ length: charCodes.length }).map((_, idx) => {
+      let tempKey = this.key
+
+      if (this.key.length < charCodes.length) {
+        const diff = charCodes.length - this.key.length
+
+        tempKey = this.key.repeat(diff)
+      }
+
+      return tempKey.charCodeAt(idx) - this.FIRST_CHAR_CODE
+    })
   }
 }
+
+const simpleCipher = new SimpleCipher("abcdefghij")
+
+console.log(simpleCipher.encode("aaaaaaaaaa"));
+console.log(simpleCipher.decode("abcdefghij"));
+
